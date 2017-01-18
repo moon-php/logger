cb29b22e42583899098e486921d35ce7ee68d203
Damiano Petrungaro <damianopetrungaro@gmail.com>
2017-01-19 00:28:52 +0100

    Added RotatingHandler

---
+++ src/Handler/RotatingFileHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Moon\Logger\Handler;
+
+use Moon\Logger\Formatter\FormatterInterface;
+
+class RotatingFileHandler extends AbstractHandler
+{
+    /**
+     * @var string $filename
+     */
+    private $filename;
+    /**
+     * @var \DateInterval
+     */
+    private $rotateEvery;
+    /**
+     * @var \DateTimeImmutable
+     */
+    private $rotationStartedAt;
+
+    /**
+     * RotatingFileHandler constructor.
+     *
+     * @param FormatterInterface $formatter
+     * @param $filename
+     * @param \DateInterval $rotateEvery
+     */
+    public function __construct(FormatterInterface $formatter, $filename, \DateInterval $rotateEvery = null)
+    {
+        parent::__construct($formatter);
+        $this->filename = $filename;
+        $this->rotateEvery = $rotateEvery ?: new \DateInterval('P1D');
+        $this->rotationStartedAt = new \DateTimeImmutable('now');
+    }
+
+    /**
+     * Format the message and push it somewhere
+     *
+     * @param $name
+     * @param $level
+     * @param $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function add($name, $level, $message, array $context = [])
+    {
+        // Format the message
+        $data = $this->formatter->interpolate($name, $level, $message, $context);
+
+        // Push it to file
+        file_put_contents($this->getRotatedFilename(), $data . PHP_EOL, FILE_APPEND);
+    }
+
+    /**
+     * Return the file name appending the $this->rotationStartedAt
+     *
+     * @return string
+     */
+    private function getRotatedFilename()
+    {
+        // Check if a new file must be used
+        $now = new \DateTimeImmutable('now');
+        if ($now > $this->rotationStartedAt->add($this->rotateEvery)) {
+            $this->rotationStartedAt = $now;
+        }
+
+        // Get file name appending data by on $this->rotationEvery
+        return preg_replace('/(.*)(\.[^.]+$)/', "$1{$this->rotationStartedAt->format('Y-M-d H:m:s')}$2", $this->filename);
+    }
+}
